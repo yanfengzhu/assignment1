f7452deb31b4aef95aa30d16c45a6cff2b6a1a5e
yanfengzhu <yzhu2@mail.sfsu.edu>
2016-04-25 21:30:11 +0000

    Assignment3 Completed

--- fb.js
+++ fb.js
@@ -1,6 +1,6 @@
 // facebook app settings - fb.js
 module.exports = {
-  'appID' : '1665356590409458',
-  'appSecret' : 'f5f554727753d4ee49366919dad76699',
-  'callbackUrl' : 'https://auth-exercise-matthewlevy.c9users.io/login/facebook/callback'
+  'appID' : '967095630073181',
+  'appSecret' : '8cefe903a5a638e3daee5594be73abfd',
+  'callbackUrl' : 'https://assignment-1-yanfengzhu-1.c9users.io/login/facebook/callback'
 }
